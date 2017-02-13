e9be0b94f3c1c90ed257a0ab6de272299cfd128c
Antonin GAVREL <angavrel@e1r8p9.42.fr>
2017-02-13 13:15:57 +0100

    more

--- 3-0-add_prime_sum/add_prime_sum.c
+++ 3-0-add_prime_sum/add_prime_sum.c
@@ -6,7 +6,7 @@
 /*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/03 17:53:51 by angavrel          #+#    #+#             */
-/*   Updated: 2016/12/21 12:45:35 by angavrel         ###   ########.fr       */
+/*   Updated: 2017/02/13 13:14:50 by angavrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,31 +34,32 @@ int		ft_atoi_nc(char *s)
 	return (r);
 }
 
-int		is_prime(int n, int i)
+
+int is_prime(int n)
 {
-	while (i <= n)
-		if (n % i++ == 0)
+	int i;
+
+	i = 2;
+	while (i < n)
+		if (!(n % i++))
 			return (0);
 	return (1);
 }
 
+
 void	add_prime_sum(int n)
 {
 	int	i;
+	int	count;
 
+	count = 0;
 	if (n == 1)
-		ft_putchar('1');
-	i = 2;
-	while (n >= i)
-	{
-		if (is_prime(n, i))
-		{
-			n /= i;
-			ft_putposnbr(i);
-			i = n - 1;
-		}
-		++i;
-	}
+		count = 1;
+	i = 1;
+	while (++i <= n)
+		if (is_prime(i))
+			count += i;
+	ft_putposnbr(count);
 }
 
 int		main(int ac, char **av)
