c02417bdaa45c97f5d394b294e61521d75402ae2
Antonin GAVREL <angavrel@e2r9p8.42.fr>
2017-02-27 13:21:06 +0100

    brackets brackets

--- Grade_Me/brackets/brackets.c
+++ Grade_Me/brackets/brackets.c
@@ -5,44 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/23 03:32:55 by angavrel          #+#    #+#             */
-/*   Updated: 2017/02/26 23:51:43 by angavrel         ###   ########.fr       */
+/*   Created: 2017/02/27 13:18:53 by angavrel          #+#    #+#             */
+/*   Updated: 2017/02/27 13:18:58 by angavrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-** Many thanks to Anselme for his original idea :
-** https://github.com/grumbach/misc/blob/master/brackets/brackets.c
-*/
-
 #include <unistd.h>
 
-int	braclose(char *str, char c, int i, int b)
+int		bracketsclose(char *s, char c, int b, int i)
 {
-	while (b && *(++str) && (i++))
-		if (*str == c || *str == c + c % 2 + 1)
-			*str == c ? ++b : --b;
+	while (b && *(++s) && i++)
+		if (*s == c || *s == c + c % 2 + 1)
+			*s == c ? ++b : --b;
 	return (i);
 }
 
-int	brackets(char *str, char c)
+int		brackets(char *s, char c)
 {
-	if (*str == c)
+	if (*s == c)
 		return (1);
-	else if (!*str || *str == ')' || *str == '}' || *str == ']')
+	else if (!*s || *s == ')' || *s == '}' || *s == ']')
 		return (0);
-	else if (*str == '(' || *str == '{' || *str == '[')
-		return (brackets(str + 1, *str + *str % 2 + 1)
-			* brackets(str + braclose(str, *str, 1, 1), c));
+	else if (*s == '(' || *s == '{' || *s =='[')
+		return (brackets(s + 1, *s + *s % 2 + 1)
+				* brackets(s + bracketsclose(s, *s, 1, 1), c));
 	else
-		return (brackets(str + 1, c));
+		return (brackets(s + 1, c));
 }
 
-int	main(int ac, char **av)
+int		main(int ac, char **av)
 {
-	int	i;
+	int i = 0;
 
-	i = 0;
 	if (ac > 1)
 		while (++i < ac)
 			brackets(av[i], 0) ? write(1, "OK\n", 3) : write(1, "Error\n", 6);
