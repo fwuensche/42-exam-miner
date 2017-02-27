9ea0732de240edd88e1bfc3211160ca4e8dfbd16
Antonin GAVREL <angavrel@e2r9p8.42.fr>
2017-02-27 11:30:48 +0100

    missing brackets

--- Grade_Me/first_word/first_word.c
+++ Grade_Me/first_word/first_word.c
@@ -6,7 +6,7 @@
 /*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/27 11:20:43 by angavrel          #+#    #+#             */
-/*   Updated: 2017/02/27 11:22:29 by angavrel         ###   ########.fr       */
+/*   Updated: 2017/02/27 11:30:33 by angavrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ void	ft_putchar(char c)
 int		main(int ac, char **av)
 {
 	if (ac == 2)
-		while (*av[1] == ' ' || *av[1] == '\t')
+	{
+		while (*av[1] && (*av[1] == ' ' || *av[1] == '\t'))
 			++av[1];
-		while (*av[1] && (*av[1] != ' ' && *av[1] != '\t'))
+		while (*av[1] != '\0' && (*av[1] != ' ' && *av[1] != '\t'))
 			ft_putchar(*av[1]++);
+	}
 	ft_putchar('\n');
 	return (0);
 }
