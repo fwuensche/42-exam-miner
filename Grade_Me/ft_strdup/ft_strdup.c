5afdc28c63944ca7a04c1e08622dd3264865cbe8
Antonin GAVREL <angavrel@e2r9p8.42.fr>
2017-02-27 11:33:15 +0100

    strdup

---
+++ Grade_Me/ft_strdup/ft_strdup.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/06 18:22:00 by angavrel          #+#    #+#             */
+/*   Updated: 2017/02/27 11:32:30 by angavrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*s;
+	int		len;
+
+	while (src[len])
+		++len;
+	if (!(s = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	s[len] = '\0';
+	while (len--)
+		s[len] = src[len];
+	return (s);
+}
