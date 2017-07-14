5950ca50a79af414922a687677e0dc227629f118
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-14 12:30:10 -0700

    Simplify aff_last_param

--- 0-1-aff_last_param/aff_last_param.c
+++ 0-1-aff_last_param/aff_last_param.c
@@ -3,24 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   aff_last_param.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: angavrel <angavrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 17:46:21 by angavrel          #+#    #+#             */
-/*   Updated: 2016/12/07 17:47:34 by angavrel         ###   ########.fr       */
+/*   Updated: 2017/07/14 12:29:50 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putstr(char *str)
-{
-	while (*str)
-		write(1, str++, 1);
-}
-
 int		main(int ac, char **av)
 {
 	if (ac > 1)
-		ft_putstr(av[ac - 1]);
+		while (*av[ac - 1])
+			write(1, av[ac - 1]++, 1);
 	write(1, "\n", 1);
+	return (0);
 }
