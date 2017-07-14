bad641ffb71bfa8ce8bae1bfbc660478723768c6
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-14 11:57:53 -0700

    Simplify maff_alpha

--- 0-0-maff_alpha/maff_alpha.c
+++ 0-0-maff_alpha/maff_alpha.c
@@ -3,27 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   maff_alpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: angavrel <angavrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/21 11:26:55 by angavrel          #+#    #+#             */
-/*   Updated: 2016/12/21 11:27:58 by angavrel         ###   ########.fr       */
+/*   Updated: 2017/07/14 11:57:42 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 int		main(void)
 {
-	int i;
-
-	i = 96;
-	while (i++ < 122)
-		(i % 2 == 0) ? ft_putchar(i - 32) : ft_putchar(i);
-	ft_putchar('\n');
+	write(1, "aBcDeFgHiJkLmNoPqRsTuVwXyZ\n", 27);
 	return (0);
 }
