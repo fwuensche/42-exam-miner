779cd5ef13624a88ad3feb0e2e6c0dcae9ed4317
Flavio Wuensche <fwuensch@student.42.us.org>
2017-07-14 11:56:04 -0700

    Simplify print numbers

--- 0-0-ft_print_numbers/ft_print_numbers.c
+++ 0-0-ft_print_numbers/ft_print_numbers.c
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_numbers.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/03 20:34:55 by angavrel          #+#    #+#             */
-/*   Updated: 2016/12/03 20:41:10 by angavrel         ###   ########.fr       */
+/*   Created: 2017/07/14 11:55:29 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/14 11:55:56 by fwuensch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+int		main(void)
 {
+	char c;
+
+	c = '0';
+	while (c <= '9')
+	{
 		write(1, &c, 1);
-}
-
-void	ft_print_numbers(void)
-{
-		int i;
-
-		i = 0;
-		while (i < 10)
-			ft_putchar(i++ + '0');
+		c++;
+	}
+	write(1, "\n", 1);
+	return (0);
 }
